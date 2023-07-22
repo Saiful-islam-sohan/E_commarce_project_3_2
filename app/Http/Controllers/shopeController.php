5f67708d08d2dd7685e659ea4d9d00848ca7889e
Saiful Islam sohan <saifulislam.cse.pust@gmail.com>
2023-07-22 23:43:32 +0600

    product show

--- app/Http/Controllers/shopeController.php
+++ app/Http/Controllers/shopeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -9,11 +10,17 @@ class shopeController extends Controller
 {
     public function index(){
         $categories = Category::where('is_active', 1)
+        ->with('')
         ->latest('id')
         ->limit(10)
-        ->select(['id', 'title'])
+        ->select(['id', 'title','slug'])
         ->get();
-        return view('frontend.pages.shope',compact('categories'));
+
+        $products=Product::where('is_active',1)->latest('id')
+        ->select(['id','name','slug','product_price','product_off_price','short_discription',
+        'delivary','long_discription_up','short_discription_down','product_image','product_rating'])
+        ->paginate(12);
+        return view('frontend.pages.shope',compact('categories','products'));
     }
 
 
